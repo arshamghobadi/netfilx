670ee5b33405a6d64b09a72fc16631bb0d190fee
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-03-13 23:51:42 +0000

    create input tage share and login and register ui

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,20 @@
 import Image from 'next/image';
+import { useCallback, useState } from 'react';
+import Inputs from '../components/Inputs';
 
 type Props = {};
 
-function auth({}: Props) {
+function Auth({}: Props) {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [variant, setVariant] = useState('login');
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariiant) =>
+      currentVariiant === 'login' ? 'register' : 'login'
+    );
+  }, []);
   return (
     <div className=" relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -17,7 +29,48 @@ function auth({}: Props) {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2- lg:w-w/5 lg:max-w-md rounded-md w-full">
-            <h2 className=" text-white text-4xl mb-8 font-semibold">Sign in</h2>
+            <h2 className=" text-white text-4xl mb-8 font-semibold">
+              {variant === 'login' ? 'Sign in' : 'register'}
+            </h2>
+            <div className=" flex flex-col gap-4">
+              {variant === 'register' && (
+                <Inputs
+                  lable="username"
+                  onChange={(e: any) => setName(e.target.value)}
+                  id="name"
+                  value={name}
+                />
+              )}
+              <Inputs
+                lable="Email"
+                onChange={(e: any) => setEmail(e.target.value)}
+                id="email"
+                type="email"
+                value={email}
+              />
+              <Inputs
+                lable="password"
+                onChange={(e: any) => setPassword(e.target.value)}
+                id="password"
+                type="password"
+                value={password}
+              />
+            </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === 'login' ? 'Login' : 'Sign up'}
+            </button>
+            <p className=" text-neutral-500 mt-12">
+              {variant === 'login'
+                ? 'First time using Netflix?'
+                : 'Already have and acount?'}
+
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === 'login' ? 'Create an acount' : 'Login'}
+              </span>
+            </p>
           </div>
         </div>
       </div>
@@ -25,4 +78,4 @@ function auth({}: Props) {
   );
 }
 
-export default auth;
+export default Auth;
